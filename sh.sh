b4c6b381b48093cfc4d0d78367e7b28b764d06a6
tahmid-404-20 <1905002@ugrad.cse.buet.ac.bd>
2023-11-23 18:11:20 +0600

    offline1-key schedule done

---
+++ sh.sh
@@ -0,0 +1,7 @@
+echo "# CSE406-ComputerSecuritySessional" >> README.md
+  git init
+  git add README.md
+  git commit -m "first commit"
+  git branch -M main
+  git remote add origin https://github.com/tahmid-404-20/CSE406-ComputerSecuritySessional.git
+  git push -u origin main
